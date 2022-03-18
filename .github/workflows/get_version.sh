6932f1dd08557226eb66c5458b47d419f2ca87ee
JonnyB <jboeckel@outlook.com>
2022-03-18 11:16:51 +0000

    FIx get_version script

--- .github/workflows/get_version.sh
+++ .github/workflows/get_version.sh
@@ -3,10 +3,9 @@
 IMAGE=${1:-"localimage:test"}
 
 get_version () {
-    # Command will be executed in the built container and should return the version without v (e.G. 1.2.3, not v1.2.3) 
     COMMAND='/app/code-server/bin/code-server --version | tail -n1 | cut -d" " -f1'
 
-    _VERSION=$(docker run --rm $IMAGE sh -c "$COMMAND")
+    _VERSION=$(docker run --rm $IMAGE sh -c "$COMMAND" | grep -oP "\d+\.\d+\.\d+")
     echo "v$_VERSION"
 }
 
